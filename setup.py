230b993cfd15a2f2fe8c094aaefa3ba52867abcc
xrotwang <xrotwang@googlemail.com>
2017-11-17 11:59:51 +0100

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clld-glottologfamily-plugin',
-    version='2.0.1',
+    version='2.1.0',
     description='clld-glottologfamily-plugin',
     classifiers=[
         "Programming Language :: Python",
